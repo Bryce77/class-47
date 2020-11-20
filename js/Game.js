29be91541539f86ad6c6a02747085983c9a9551c
Bryce77 <71152555+Bryce77@users.noreply.github.com>
2020-11-20 14:01:45 -0800

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,104 @@
+class Game{
+
+constructor(){
+
+}
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    });
+  }
+
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form();
+      form.display();
+        manThing = createSprite(200,displayHeight-230,50,50);
+      manThing.scale = 0.2;
+      manThing.addImage(man);
+
+      
+    }
+
+
+}
+
+
+play(){
+  form.hide();
+
+  edges = createEdgeSprites();
+
+manThing.collide(edges[3]);
+manThing.bounceOff(edges[0]);
+manThing.bounceOff(edges[1]);
+manThing.bounceOff(edges[2]);
+manThing.bounceOff(edges[3]);
+
+  console.log(manThing.y);
+
+
+  Player.getPlayerInfo();
+  
+  if(allPlayers !== undefined){
+   background(jungle);
+   
+
+
+ 
+
+    //key is down
+
+    if(keyIsDown(87) && manThing.y >112){
+     // manThing.velocityY = -9;
+     // manThing.velocityX = 0;
+      manThing.y = manThing.y - 5;
+
+     } 
+     
+     
+         if(keyIsDown(68)){
+      // manThing.velocityX = 9;
+     //  manThing.velocityY = 0;
+      }
+     
+     
+          if(keyIsDown(83)){
+      // manThing.velocityY = 9;
+     //  manThing.velocityX = 0;
+      } 
+     
+     
+          if(keyIsDown(65)){
+      // manThing.velocityX = -9;
+      // manThing.velocityY = 0;
+       } 
+ ///////////////////////////////////////////
+
+
+//if(keyDown("space") && trex.y >= 359){
+ // trex.velocityY = -12 ;  }
+
+drawSprites();
+
+  }
+
+  //drawSprites();
+}
+
+  }
